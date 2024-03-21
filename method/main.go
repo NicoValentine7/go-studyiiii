fd6b565ec6fb4cda7922a2bdf28a95135e5ca6b6
Nico <50232024+NicoValentine7@users.noreply.github.com>
2024-03-21 14:01:14 +0900

    add method quiz

---
+++ method/main.go
@@ -0,0 +1,27 @@
+package main
+
+// 1. "Rectangle"という名前の構造体を定義する
+//    フィールドは以下の通り
+//    - Width: float64
+//    - Height: float64
+
+// 2. "NewRectangle"というコンストラクタ関数を定義する
+//    引数でWidthとHeightを受け取り、Rectangleのインスタンスを返す
+
+// 3. "Rectangle"構造体に"Area"というメソッドを定義する
+//    メソッドは長方形の面積を計算して返す
+
+// 4. "Rectangle"構造体に"Perimeter"というメソッドを定義する
+//    メソッドは長方形の周囲の長さを計算して返す
+
+func main() {
+    // 5. NewRectangleを使って、Rectangleのインスタンスを作成する
+    //    幅: 5.0, 高さ: 3.0
+
+    // 6. 作成したインスタンスのWidthとHeightを出力する
+
+    // 7. 作成したインスタンスのAreaメソッドを呼び出して、面積を出力する
+
+    // 8. 作成したインスタンスのPerimeterメソッドを呼び出して、周囲の長さを出力する
+
+}
